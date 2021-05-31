15024f01290bb656998294a9d5e33d54743950d5
Delphik <max.henny@gmail.com>
2021-05-30 20:12:33 -0400

    Add files via upload

--- wow-bc.sh
+++ wow-bc.sh
@@ -2,5 +2,8 @@
 # modified laucher for Wow Classic
 # author macx henny
 # License MIT
+# Dependencies, Proton, Wine, Feral Gamemode, Battlenet, Wow Classic
+# https://old.reddit.com/r/Blizzard/comments/7aywrt/all_current_shortcut_codes_for_launching_games/
 
-gamemoderun proton '/home/maxh/Games/battlenet/drive_c/Program Files (x86)/World of Warcraft/_classic_'
+gamemoderun wine '/home/maxh/Games/battlenet/drive_c/Program Files (x86)/Battle.net'
+gamemoderun wine '/home/maxh/Games/battlenet/drive_c/Program Files (x86)/World of Warcraft/_classic_'
