7d512be1fa5f69600b5e5a10b63bbd469aef6035
Delphik <max.henny@gmail.com>
2021-07-31 19:11:33 -0400

    Added Pamac support

--- mjup.sh
+++ mjup.sh
@@ -6,11 +6,21 @@
 
 set -e
 
-#Updates with Pacman by default (yay if passed the -y flag) and flatpak
+#Updates with Pacman by default (yay if passed the -y flag) and then flatpak
+# -p flag uses pamac, -pa pamac with AUR
 
 if [ "$1" == "-y" ]; then
     echo "Updating Repos and AUR with yay" | cowsay | lolcat
     yay -Syu -y
+
+elif [ "$1" == "-p" ]; then
+    echo "Updating Repos with Pamac" | cowsay | lolcat
+    sudo pamac update --no-confirm
+    
+elif [ "$1" == "-pa" ]; then
+    echo "Updating Repos and AUR with Pamac" | cowsay | lolcat
+    sudo pamac update -a --no-confirm
+      
 else
     echo "Updating Repos with pacman" | cowsay | lolcat
     sudo pacman -Syu 
@@ -21,3 +31,4 @@ sudo flatpak update -y
 
 echo "Updates Complete" | cowsay | lolcat
 exit
+
