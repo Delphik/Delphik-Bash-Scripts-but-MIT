7606796893abdae52560af68ac5f8bb0fe0a4031
Delphik <max.henny@gmail.com>
2021-08-07 19:40:32 -0400

    Moved Flatpak update to its own function

--- mjup.sh
+++ mjup.sh
@@ -7,27 +7,36 @@
 set -e
 
 #Updates with Pacman by default (yay if passed the -y flag) and then flatpak
-# -p flag uses pamac, -pa pamac with AUR
+# -p flag uses pamac, -pa pamac with AUR flag
+
+flatpak_update () {
+
+echo "Updating Flatpak" | cowsay | lolcat
+sudo flatpak update -y
+
+}
 
 if [ "$1" == "-y" ]; then
     echo "Updating Repos and AUR with yay" | cowsay | lolcat
     yay -Syu -y
+    flatpak_update
 
 elif [ "$1" == "-p" ]; then
     echo "Updating Repos with Pamac" | cowsay | lolcat
     sudo pamac update --no-confirm
-    
+    flatpak_update
+
 elif [ "$1" == "-pa" ]; then
     echo "Updating Repos and AUR with Pamac" | cowsay | lolcat
     sudo pamac update -a --no-confirm
-      
+    flatpak_update
+
 else
     echo "Updating Repos with pacman" | cowsay | lolcat
     sudo pacman -Syu 
+    flatpak_update
 fi
 
-echo "Updating Flatpak" | cowsay | lolcat
-sudo flatpak update -y
 
 echo "Updates Complete" | cowsay | lolcat
 exit
