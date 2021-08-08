a031a26726e8b60f5d19bedb8d2d8e311eeed155
Delphik <max.henny@gmail.com>
2021-08-07 20:34:11 -0400

    Keyring Help and Other Improvments
    * moved pacman update to function
    * added -k keyring refresh option
    * added -h help option
    * lolcatted functions

--- mjup.sh
+++ mjup.sh
@@ -3,23 +3,46 @@
 #Updater for Manjaro
 
 # Set BASH to quit script and exit on errors:
+# Dependencies: pamac, yay, flatpak, lolcat, cowsay
 
 set -e
 
-#Updates with Pacman by default (yay if passed the -y flag) and then flatpak
-# -p flag uses pamac, -pa pamac with AUR flag
+#Updates with Pacman by default and then flatpak
+# -y        Updates via Yay
+# -p        Updates via pamac 
+# -pa       Updates via pamac with AUR flag
+# -k        Refreshes pacman keyring then updates via flatpak
+# -h        Help
 
 flatpak_update () {
 
-echo "Updating Flatpak" | cowsay | lolcat
-sudo flatpak update -y
+    echo "Updating Flatpak" | cowsay | lolcat
+    sudo flatpak update -y | lolcat
 
+}
+
+pacman_update () {
+    
+    echo "Updating Repos with pacman" | cowsay | lolcat
+    sudo pacman -Syu |lolcat
 }
 
 if [ "$1" == "-y" ]; then
     echo "Updating Repos and AUR with yay" | cowsay | lolcat
     yay -Syu -y
     flatpak_update
+
+
+elif [ "$1" == "-h" ]; then
+    echo "Mjup Help" | cowsay | lolcat
+    echo
+    echo "Updates with Pacman by default and then flatpak" | lolcat
+    echo " -y        Updates via Yay" | lolcat
+    echo " -p        Updates via pamac"  | lolcat
+    echo " -pa       Updates via pamac with AUR flag" | lolcat
+    echo " -k        Refreshes pacman keyring then updates via flatpak" | lolcat
+    echo " -h        Help" | lolcat
+    exit
 
 elif [ "$1" == "-p" ]; then
     echo "Updating Repos with Pamac" | cowsay | lolcat
@@ -31,9 +54,12 @@ elif [ "$1" == "-pa" ]; then
     sudo pamac update -a --no-confirm
     flatpak_update
 
+elif [ "$1" == "-k" ]; then
+    echo "Refreshing Pacman Keyring" | cowsay | lolcat
+    sudo pacman-key --populate archlinux
+    pacman_update
 else
-    echo "Updating Repos with pacman" | cowsay | lolcat
-    sudo pacman -Syu 
+    pacman_update 
     flatpak_update
 fi
 
